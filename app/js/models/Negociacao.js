edc72dcafbbe0faaa444f89fabbe07d4362d9e7e
Assisneto <netodamasceno12@gmail.com>
2019-01-05 11:54:48 -0300

    hardCode

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,16 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._data;
+    }
+    get valor() {
+        return this._data;
+    }
+}
